0a86fa199bba28c1b1eea62251d44e1232154125
realthrongler <fjack6677@gmail.com>
2025-04-23 09:15:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,53 +13,64 @@ words = []
 for i in range(0, 2309):
     word = file.readline().strip()
     words.append(word)
+file.close()
 
+def main():
+    #choosing a random word from the list
+    plaintext_word = words[random.randint(0, 2309)]
+    chosen_word = list(plaintext_word)
 
-#choosing a random word from the list
-plaintext_word = words[random.randint(0, 2309)]
-chosen_word = list(plaintext_word)
+    guesses = 6
 
-guesses = 6
+    #The main part of the program, the main character, the bigger fish to fry, the whole enchilada, the man the myth the legend, head honcho
+    while guesses != 0:
+        guessed_word = input("What word do you guess?\n")
+        #word for checking their input, it's a list for easier comparison of each letter, will be modified later on for that strange double rule
+        checking_word = list(guessed_word)
+        #word for checking input
+        original_word = list(guessed_word)
+        #error handling
+        if guessed_word.strip().lower() not in words:
+            print("Sorry! That word in invalid, please try again!\n")
+            continue
+        #Checking for the win condition
+        elif guessed_word.strip().lower() in words:
+            if checking_word == chosen_word or checking_word == list("nagra"):
+                print("Congratulations! You won!")
+                break
+                quit()
+            else:
+                output = ""
+                for char in range(5):
+                    for thong in range(5):
+                        if original_word[thong] == chosen_word[thong]:
+                            duplicates = original_word[thong] #Saving the correct letter to check for duplicates
+                            checking_word[thong] = "@" #Marking as correct
+                            if duplicates in checking_word: 
+                                #Marking all duplicates as incorrect
+                                checking_word[checking_word.index(duplicates)] = "$"
+                    print(guesses)
+                    if checking_word[char] == "@":
+                        #Checking for correct letters
+                        output += colorama.Back.GREEN + original_word[char] + colorama.Style.RESET_ALL + " "
+                    elif checking_word[char] in chosen_word:
+                        #Checking for letters in the word, but in the wrong spot
+                        output += colorama.Back.YELLOW + original_word[char] + colorama.Style.RESET_ALL + " "
+                    #Guesses reaching 0 not working properly, had to add this in to break the loop
+                    elif guesses <= 0:
+                        break
+                    else:
+                        #All other cases (The letter is not correct)
+                        output += original_word[char] + " "
+                print(output)
+                guesses -= 1
+    if guesses == 0:
+        #Lost game :(
+        print("Oh no! You ran out of guesses! The word was: " + plaintext_word)
+        again = str(input("Do you want to play again? y/n\n"))
+        #Checking if they want to play again (I did not read the rubric)
+        if again.lower().strip() == "y":
+            main()
 
-#The main part of the program, the main character, the bigger fish to fry, the whole enchilada, the man the myth the legend, head honcho
-while guesses != 0:
-    guessed_word = input("What word do you guess?\n")
-    #word for checking their input, it's a list for easier comparison of each letter, will be modified later on for that strange double rule
-    checking_word = list(guessed_word)
-    #word for checking input
-    original_word = list(guessed_word)
-    #error handling
-    if guessed_word.strip().lower() not in words:
-        print("Sorry! That word in invalid, please try again!\n")
-        continue
-    #Checking for the win condition
-    elif guessed_word.strip().lower() in words:
-        if checking_word == chosen_word or checking_word == list("nagra"):
-            print("Congratulations! You won!")
-            break
-            quit()
-        else:
-            output = ""
-            for char in range(5):
-                for thong in range(5):
-                    if original_word[thong] == chosen_word[thong]:
-                        duplicates = original_word[thong] #Saving the correct letter to check for duplicates
-                        checking_word[thong] = "@" #Marking as correct
-                        if duplicates in checking_word: 
-                            #Marking all duplicates as incorrect
-                            checking_word[checking_word.index(duplicates)] = "$"
-                if checking_word[char] == "@":
-                    #Checking for correct letters
-                    output += colorama.Back.GREEN + original_word[char] + colorama.Style.RESET_ALL + " "
-                    guesses -= 1
-                elif checking_word[char] in chosen_word:
-                    #Checking for letters in the word, but in the wrong spot
-                    output += colorama.Back.YELLOW + original_word[char] + colorama.Style.RESET_ALL + " "
-                    guesses -= 1
-                else:
-                    #All other cases (The letter is not correct)
-                    output += original_word[char] + " "
-                    guesses -= 1
-            print(output)
-if guesses == 0:
-    print("Oh no! You ran out of guesses! The word was: " + plaintext_word)
+#Calling the function for the program to actually start
+main()
